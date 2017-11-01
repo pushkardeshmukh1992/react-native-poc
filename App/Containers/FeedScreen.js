4b229ce624eb4b0d6d3704ae05e6f6f75bb929b3
sushant <sushantj@sminq.com>
2017-11-01 06:18:22 +0530

    Styled FeedScreen.js a bit more.

--- App/Containers/FeedScreen.js
+++ App/Containers/FeedScreen.js
@@ -46,28 +46,33 @@ export default class FeedScreen extends Component {
       <View style={styles.mainContainer}>
         <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
         <ScrollView style={styles.container}>
-          <View style={styles.centered}>
+          {/* <View style={styles.centered}>
             <Text style={{ color: 'black'}}>Feed Screen</Text>
-          </View>
+          </View> */}
           <View style={{ flex: 1, paddingTop: 10 }}>
             <ListView
               dataSource={this.state.dataSource}
               renderRow={(rowData) =>
                 <View style={{ flex: 1, flexDirection: 'row',  borderColor: '#CECECE', borderStyle: 'solid', borderBottomWidth: 1.0, paddingVertical: 16 }}>
-                  <Image source={{ uri: rowData.image ? rowData.image : '../Images/ir.png' }} style={{ flex: 2, height: 80, width: 50, borderRadius: 100, marginHorizontal: 10 }} />
+                  <Image source={{ uri: rowData.image ? rowData.image : '../Images/ir.png' }} style={{ flex: 3, height: 110, width: 50, borderRadius: 100, marginHorizontal: 10 }} />
                   <View style={{ flexDirection: 'column', flex: 6 }}>
-                    <Text style={{ fontSize: 20, color: 'black', paddingHorizontal: 8 }}>{rowData.businessName}</Text>
-                    <Text style={{ color: '#aaa', fontSize: 18, letterSpacing: 2, paddingHorizontal: 8 }}> {rowData.verticalType.toUpperCase()}</Text>
-                    <Text numberOfLines={1} style={{ color: 'black', paddingHorizontal: 8 }}> {rowData.address}</Text>
-                    <View style={{ flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'space-between' }}>
-                      <Image source={ require('../Images/Emoticons-05-512.png') } style={{ width: 20, height: 20, alignSelf: 'flex-end' }} />
-                      <Text style={{ fontWeight: 'bold', color: 'black', paddingHorizontal: 8 }}> Crowded </Text>
-                      <Text style={{ fontWeight: '300', alignSelf: 'flex-end', fontSize: 10, color: 'grey', paddingHorizontal: 8 }}> 23 minutes ago </Text>
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 6 }}>
+                      <Text style={{ fontSize: 20, color: 'black' }}>{rowData.businessName}</Text>
+                      <Image source={require('../Images/39.Heart-512.png') } style={{ width: 20, height: 20, alignSelf: 'flex-end' }} />
                     </View>
-                    <Button
-                      onPress={() => this.props.navigation.navigate('PlaceDetailsScreen', { name: rowData.businessName })}
-                      title="Details"
-                    />
+                    <Text style={{ color: '#aaa', fontSize: 18, paddingHorizontal: 4 }}> {rowData.verticalType.toUpperCase()}</Text>
+                    <Text numberOfLines={1} style={{ color: 'black', paddingHorizontal: 8 }}> {rowData.address}</Text>
+                    <View style={{ flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'space-between', padding: 8 }}>
+                      <Image source={ require('../Images/Emoticons-05-512.png') } style={{ width: 20, height: 20, alignSelf: 'flex-end' }} />
+                      <Text style={{ fontWeight: 'bold', color: 'black' }}> Crowded </Text>
+                      <Text style={{ fontWeight: '300', alignSelf: 'flex-end', fontSize: 10, color: 'grey' }}> 23 minutes ago </Text>
+                    </View>
+                    <View style={{ paddingHorizontal: 8 }}>
+                      <Button
+                        onPress={() => this.props.navigation.navigate('PlaceDetailsScreen', { name: rowData.businessName })}
+                        title="Details"
+                      />
+                    </View>
                   </View>
               </View>}
             />
